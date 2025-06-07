754d5b5d228b6d1544f73b3b1eceb5de87491e33
rohittiwariofficial <kacs.rohit.1994@gmail.com>
2025-06-07 22:54:43 +0530

    security: fixed vulnerabilities

--- src/config/db.mongo.ts
+++ src/config/db.mongo.ts
@@ -1,8 +1,15 @@
 import mongoose from 'mongoose';
 
 export const connectMongo = async () => {
+  const mongoURI = process.env.MONGO_URI;
+  
+  if (!mongoURI) {
+    console.error('❌ MONGO_URI not defined in environment variables.');
+    process.exit(1);
+  }
+
   try {
-    await mongoose.connect(process.env.MONGO_URI!);
+    await mongoose.connect(mongoURI);
     console.log('✅ MongoDB connected');
   } catch (err) {
     if (err instanceof Error) {
--- src/config/db.redis.ts
+++ src/config/db.redis.ts
@@ -1,6 +1,10 @@
 import Redis from 'ioredis';
 
-const redis = new Redis(); // defaults to localhost:6379
+const redis = new Redis({
+  host: process.env.REDIS_HOST || '127.0.0.1',
+  port: Number(process.env.REDIS_PORT) || 6379,
+  password: process.env.REDIS_PASSWORD || undefined,
+});
 
 redis.on('connect', () => console.log('✅ Redis connected'));
 redis.on('error', err => console.error('❌ Redis error:', err));
